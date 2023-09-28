13a1eb42e0290ffc665c7fcef7045c491cc35852
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-09-28 10:28:51 -0600

    set up initial barbarian proficiencies and layout of skills to take proficiencies in

--- lib/models/dndClasses.py
+++ lib/models/dndClasses.py
@@ -1,16 +1,34 @@
-
+import inquirer, time
+from charProficiencies import CharProficiencies
 
 class Barbarian():
     hit_dice_val = 12
 
-    # @classmethod
-    # def start_proficiencies(cls, charID):
-    #     sql = f'''
-    #     INSERT INTO charProficiencies (character_rel, name)
-    #     VALUES ("{charID}", "Light Armor"),
-    #            ("{charID}", "Light Armor"),
-    #            ("{charID}", "Light Armor"),
-    #            ("{charID}", "Light Armor"),
-    #     '''
-    #     CURSOR.execute(sql)
-    #     CONN.commit()
+    @classmethod
+    def start_proficencies(cls, charID):
+        lightArmor = CharProficiencies(charID, 46)
+        mediumArmor = CharProficiencies(charID, 56)
+        shields = CharProficiencies(charID, 80)
+        simpleWeapons = CharProficiencies(charID, 84)
+        martialWeapons = CharProficiencies(charID, 53)
+    
+    @classmethod
+    def skill_proficiencies(cls, charID):
+        questions = [
+        inquirer.Checkbox('option',
+                      message = "Which skills would you like to be proficient in (Pick 2)",
+                      choices = ["animal handling", "athletics", "intimidation", "nature", "perception", "survival"],
+                      ),
+        ]
+        answer = inquirer.prompt(questions)
+        if len(answer["option"]) == 2:
+            pass
+        elif len(answer["option"]) > 2:
+            print("Only Pick 2 Options!")
+            time.sleep(2)
+            Barbarian.skill_proficiencies(charID)
+        elif len(answer["option"]) < 2:
+            print("Please Pick 2 Options!")
+            time.sleep(2)
+            Barbarian.skill_proficiencies(charID)
+    
